3aa71a208b5e630026b84fd838d515c8bd0a6224
IdzanamiMao <tksalsk@gmail.com>
2023-02-12 21:52:30 +0300

    feat: add closing login popup when clicking outside the popup

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 const menuIcon = document.querySelector('.nav');
 const navbar = document.querySelector('.mobile-nav');
 const overlay = document.querySelector('.mobile-menu');
-const exit = document.querySelector('.mobile-nav__item');
 
 menuIcon.addEventListener('click', () => {
   navbar.classList.add("change");
-  overlay.classList.add("active");
+   overlay.classList.add("active");
 });
 
-overlay.addEventListener('click', () => {
+ overlay.addEventListener('click', () => {
   navbar.classList.remove("change");
   overlay.classList.remove("active");
 });
 
 
+
+
+/* ---------- Login pop up / Sign up pop up ---------- */
 const login = document.querySelector('.button__login');
 const popUp = document.querySelector('.login__pop-up');
 
 login.addEventListener('click', () => {
   popUp.classList.toggle("visible");
 
+  document.addEventListener('click', e => {
+    if(!login.contains(e.target)) {
+      popUp.classList.remove("visible");
+    };
+  })
 });
-
-
-
 
 const register = document.querySelector('.register');
 
